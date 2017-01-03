9791a825988058ae9799d1a46d116b9785928a90
ALexander4295502 <yuanzhengstl@gmail.com>
2017-01-02 22:58:32 -0600

    Unity 1st commit
    Unity 1st commit

---
+++ Survival Shooter/Assets/Managers/EnemyManager.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+
+public class EnemyManager : MonoBehaviour
+{
+    public PlayerHealth playerHealth;
+    public GameObject enemy;
+    public float spawnTime = 3f;
+    public float spawnRepeatTime = 3f;
+    public Transform[] spawnPoints;
+
+
+    void Start ()
+    {
+        //Invokes the method methodName in spawnTime seconds, then repeatedly every spawnRepeatTime seconds.
+        InvokeRepeating("Spawn", spawnTime, spawnRepeatTime);
+    }
+
+
+    void Spawn ()
+    {
+        if(playerHealth.currentHealth <= 0f)
+        {
+            return;
+        }
+
+        int spawnPointIndex = Random.Range (0, spawnPoints.Length);
+
+        Instantiate (enemy, spawnPoints[spawnPointIndex].position, spawnPoints[spawnPointIndex].rotation);
+    }
+}
